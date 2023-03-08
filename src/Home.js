29e996464035f5245d28cb018e7d82e6bd986692
Macbook Air <Mac@Liyana-JS.local>
2023-03-08 23:48:15 +0800

    add changes in Home component for 1st Test

--- src/Home.js
+++ src/Home.js
@@ -6,24 +6,40 @@ function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
 
+  const onInputChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
+        <div>
+          <NavLink to="/pokedex">
+            <img
+              hidden={text === "Ready!" ? isReady : !isReady}
+              src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+              className="App-logo"
+              alt="logo"
+              style={{ padding: "15px" }}
+            />
+          </NavLink>
+        </div>
         <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
         </b>
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={text}
+          onChange={onInputChange}
+        />
+        <span style={{ color: "red" }}>
+          {text === "Ready!" ? "" : "I am not ready yet!"}
+        </span>
       </header>
     </div>
   );
